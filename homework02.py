9c8b5a4106decbe7ab89d9436169b8c0bf083abd
Eminemily <55164728+Eminemily@users.noreply.github.com>
2019-10-02 14:29:47 -0700

    Add files via upload

---
+++ homework02.py
@@ -0,0 +1,6 @@
+import random
+red = ['Bobby', 'Joey', 'Sarah', 'Jen']
+green = ['eating', 'climbing', 'swimming', 'running']
+yellow = ['park', 'house', 'pool', 'school']
+message = (random.choice(red)) + " is " + (random.choice(green)) + " at the " + (random.choice(yellow))
+print (message)
